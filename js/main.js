df4a267f7d1ca991d16f6ea1e011ff70a8b02045
Jacob Spade <spade.jacob@yahoo.com>
2018-05-07 14:48:50 -0700

    created game logic

--- js/main.js
+++ js/main.js
@@ -5,18 +5,25 @@ var cards = [
   "king",
 ];
 
-var cardOne = cards[0];
 var cardsInPlay = [];
-cardsInPlay.push(cardOne);
-console.log("User flipped queen");
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped king");
 
-if (cardsInPlay.length === 2) {
+var checkForMatch = function() {
   if (cardsInPlay[0] === cardsInPlay[1]) {
-    alert("You found a match!");
+    console.log("You found a match!");
   } else {
-      alert("Sorry, try again");
+    console.log("Sorry, try again.");
   }
 }
+
+var flipCard = function(cardId) {
+  if (cardsInPlay.length === 2) {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+      alert("You found a match!");
+    } else {
+        alert("Sorry, try again");
+    }
+  }
+  console.log("User flipped " + cards[cardId]);
+}
+flipCard(0);
+flipCard(2);
