b2f53d6574524ebbdcfef63842a14464ce860205
Jacob Spade <spade.jacob@yahoo.com>
2018-05-08 20:14:56 -0700

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,17 +1,35 @@
 var cards = [
-  "queen",
-  "queen",
-  "king",
-  "king",
+  {
+    rank: "queen",
+    suit: "hearts",
+    cardImage: "images/queen-of-hearts.png",
+  },
+  {
+    rank: "queen",
+    suit: "diamonds",
+    cardImage: "images/queen-of-diamonds.png",
+  },
+  {
+    rank: "king",
+    suit: "hearts",
+    cardImage: "images/king-of-hearts.png",
+  },
+  {
+    rank: "king",
+    suit: "diamonds",
+    cardImage: "images/king-of-diamonds.png",
+  },
 ];
 
 var cardsInPlay = [];
 
 var checkForMatch = function() {
-  if (cardsInPlay[0] === cardsInPlay[1]) {
-    console.log("You found a match!");
-  } else {
-    console.log("Sorry, try again.");
+  if (cardsInPlay.length === 2) {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+      alert("You found a match!");
+    } else {
+      alert("Sorry, try again.");
+    }
   }
 }
 
@@ -23,7 +41,11 @@ var flipCard = function(cardId) {
         alert("Sorry, try again");
     }
   }
-  console.log("User flipped " + cards[cardId]);
+  console.log("User flipped " + cards[cardId].rank);
+  cardsInPlay.push(cards[cardId].rank);
+  checkForMatch();
+  console.log(cards[cardId].cardImage);
+  console.log(cards[cardId].suit);
 }
 flipCard(0);
 flipCard(2);
